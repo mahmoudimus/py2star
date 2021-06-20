2b22235dc94eeb096cd7e3a34f3e15e5e5acf831
Mahmoud Abdelkader <mabdelkader@gmail.com>
2021-06-20 14:01:12 -0700

    checkpoint for rewriting tests

--- src/py2star/asteez/teyit.py
+++ src/py2star/asteez/teyit.py
@@ -1,8 +1,11 @@
 import argparse
 from contextlib import suppress
 from dataclasses import dataclass
+from functools import partial
 from pathlib import Path
 from typing import Callable, List, Tuple
+import re
+
 
 import libcst as cst
 import libcst.codemod
@@ -48,7 +51,246 @@ class Rewrite:
     replacement: Callable
 
 
+_codegen = cst.parse_module("")
+
+
+def arity(n):
+    def f(func):
+        func.arity = n
+        return func
+
+    return f
+
+
+@arity(2)
+def comp_op(op, lefty, righty):
+    left = _codegen.code_for_node(lefty.value)
+    right = _codegen.code_for_node(righty.value)
+    return cst.parse_expression(f"asserts.assert_that({left}).{op}({right})")
+
+
+@arity(1)
+def unary_op(op, suty):
+    """/Users/mahmoud/src/py-in-java/pycryptodome/lib/transform.py
+    /Users/mahmoud/src/py2star/transform1.py
+    Converts a method like: ``self.failUnless(True)`` to
+      asserts.assert_that(value).is_true()
+    """
+    sut = _codegen.code_for_node(suty.value)
+    return cst.parse_expression(f"asserts.assert_that({sut}).{op}()")
+
+
+@arity(5)
+def raises_op(exc_cls, *args):
+    """
+    assertRaises(exception, callable, *args, **kwds)
+    assertRaises(exception, *, msg=None)
+
+    # Test that an exception is raised when callable is called with any
+    # positional or keyword arguments that are also passed to
+    # assertRaises().
+
+    asserts.assert_fails(lambda: -1 in b("abc"), "-1 not in range")
+
+    """
+    print(args)
+    invokable = "lambda x: x"
+    regex = '".*"'
+    return cst.parse_expression(f"asserts.assert_fails({invokable}, {regex})")
+
+
+@arity(5)
+def regex_op(exc_cls, *args):
+    """
+    assertRaises(exception, callable, *args, **kwds)
+    assertRaises(exception, *, msg=None)
+
+    # Test that an exception is raised when callable is called with any
+    # positional or keyword arguments that are also passed to
+    # assertRaises().
+
+    asserts.assert_fails(lambda: -1 in b("abc"), "-1 not in range")
+
+    """
+    print(args)
+    invokable = lambda x: x
+    regex = ".*"
+    return cst.parse_expression(f"asserts.assert_fails({invokable}, {regex})")
+
+
+# def raises_op(context, exceptionClass, indent, kws, arglist, node):
+#     # asserts.assert_fails(lambda: -1 in b("abc"), "int in bytes: -1 out of range")
+#     exceptionClass.prefix = ""
+#     args = [
+#         String('"', prefix=" "),  # unquoted on purpose
+#         String(".*?"),  # match anything until the exception we are looking for
+#         String(f"{str(exceptionClass)}"),
+#     ]
+#     # this would be the second parameter
+#     # Add match keyword arg to with statement if an expected regex was provided.
+#     if "expected_regex" in kws:
+#         expected_regex = kws.get("expected_regex").clone()
+#         expected_regex.prefix = ""
+#         args.append(String(".*"))
+#         args.append(expected_regex)
+#     args.append(String('"'))  # close quote
+#
+#     # # assertRaises(exception, callable, *args, **kwds)
+#     # # assertRaises(exception, *, msg=None)
+#     # # Test that an exception is raised when callable is called with any
+#     # # positional or keyword arguments that are also passed to
+#     # # assertRaises().
+#     # # To catch any of a group of exceptions, a tuple containing the
+#     # # exception classes may be passed as exception.
+#     # with_item = Call(Name(context), args) # pytest.raises(TypeError)
+#     # with_item.prefix = " "
+#     # args = []
+#     arglist = [a.clone() for a in arglist.children[4:]]
+#     if arglist:
+#         arglist[0].prefix = ""
+#
+#     func = None
+#
+#     # :fixme: this uses hardcoded parameter names, which may change
+#     if "callableObj" in kws:
+#         func = kws["callableObj"]
+#     elif "callable_obj" in kws:
+#         func = kws["callable_obj"]
+#     elif kws["args"]:  # any arguments assigned to `*args`
+#         func = kws["args"][0]
+#     else:
+#         func = None
+#
+#     if func and func.type == syms.lambdef:
+#         suite = func.children[-1].clone()
+#     else:
+#         if func is None:
+#             # Context manager, so let us convert it to a function definition
+#             # let us create a function first.
+#             func = get_funcdef_node(
+#                 funcname=_rand(),
+#                 args=[],
+#                 body=arglist,
+#                 decorators=[],
+#                 indentation_level=1,
+#             )
+#             # append it as a child to the root node
+#             find_root(node).append_child(func)
+#             # return Node(syms.with_stmt, [with_item])
+#         # TODO: Newlines within arguments are not handled yet.
+#         # If argment prefix contains a newline, all whitespace around this
+#         # ought to be replaced by indent plus 4+1+len(func) spaces.
+#         suite = get_lambdef_node([Call(Name(str(func)), arglist)])
+#
+#     # suite.prefix = indent + (4 * " ")
+#     # new = Node(
+#     #     syms.power,
+#     #     # trailer< '.' 'is_equal_to' > trailer< '(' '2' ')' > >
+#     #     Attr(_left_asserts, Name(op))
+#     #     + [Node(syms.trailer, [LParen(), right, RParen()])],
+#     # )
+#     return Call(
+#         Name("assert_fails"),
+#         args=[suite, Comma()] + args,
+#     )
+#     # return Node(
+#     #     syms.with_stmt, [Name("with"), with_item, Name(":"), Newline(), suite]
+#     # )
+
+
+_method_map = {
+    # simple equals
+    # asserts.eq(A, B) || asserts.assert_that(A).is_equal_to(B)
+    "assertDictEqual": partial(comp_op, "is_equal_to"),
+    "assertListEqual": partial(comp_op, "is_equal_to"),
+    "assertMultiLineEqual": partial(comp_op, "is_equal_to"),
+    "assertSetEqual": partial(comp_op, "is_equal_to"),
+    "assertTupleEqual": partial(comp_op, "is_equal_to"),
+    "assertSequenceEqual": partial(comp_op, "is_equal_to"),
+    "assertEqual": partial(comp_op, "is_equal_to"),
+    "assertNotEqual": partial(comp_op, "is_not_equal_to"),
+    "assertIs": partial(comp_op, "is_equal_to"),
+    "assertGreater": partial(comp_op, "is_greater_than"),
+    "assertLessEqual": partial(comp_op, "is_lte_to"),
+    "assertLess": partial(comp_op, "is_less_than"),
+    "assertGreaterEqual": partial(comp_op, "is_gte_to"),
+    "assertIn": partial(comp_op, "is_in"),
+    "assertIsNot": partial(comp_op, "is_not_equal_to"),
+    "assertNotIn": partial(comp_op, "is_not_in"),
+    "assertIsInstance": partial(comp_op, "is_instance_of"),
+    "assertNotIsInstance": partial(comp_op, "is_not_instance_of"),
+    # unary operations
+    "assertIsNone": partial(unary_op, "is_none"),
+    "assertIsNotNone": partial(unary_op, "is_not_none"),
+    "assertFalse": partial(unary_op, "is_false"),
+    "failIf": partial(unary_op, "is_false"),
+    "assertTrue": partial(unary_op, "is_true"),
+    "failUnless": partial(unary_op, "is_true"),
+    "assert_": partial(unary_op, "is_true"),
+    # "exceptions" in larky do not exist but we have asserts.assert_fails...
+    "assertRaises": partial(raises_op, "asserts"),
+    # "assertWarns": partial(raises_op, "asserts"),
+    # # types ones
+    # "assertDictContainsSubset": partial(dual_op, "dict(\2, **\1) == \2"),
+    # "assertItemsEqual": partial(dual_op, "sorted(\1) == sorted(\2)"),
+    "assertRegex": partial(dual_op, "re.search(\2, \1)"),
+    # "assertNotRegex": partial(dual_op, "not re.search(\2, \1)"),  # new Py 3.2
+    # "assertAlmostEqual": partial(almost_op, "==", "<"),
+    # "assertNotAlmostEqual": partial(almost_op, "!=", ">"),
+    # "assertRaisesRegex": partial(RaisesRegexOp, "pytest.raises", "excinfo"),
+    # "assertWarnsRegex": partial(RaisesRegexOp, "pytest.warns", "record"),
+    # 'assertLogs': -- not to be handled here, is an context handler only
+}
+
+TEMPLATE_PATTERN = re.compile("[\1\2]|[^\1\2]+")
+
+
+# def fill_template(template, *args):
+#     parts = TEMPLATE_PATTERN.findall(template)
+#     kids = []
+#     for p in parts:
+#         if p == "":
+#             continue
+#         elif p in "\1\2\3\4\5":
+#             p = args[ord(p) - 1]
+#             p.prefix = ""
+#         else:
+#             p = cst.Name(p)
+#         kids.append(p)
+#     return kids
+#
+#
+# def DualOp(template, first, second):
+#     kids = fill_template(template, first, second)
+#     return cst.Node(syms.test, kids, prefix=" ")
+
+
+def _build_matchers() -> List[Rewrite]:
+    return [
+        # e.g. self.assertEqual(a,b) => assert a == b
+        Rewrite(
+            matcher=m.Call(
+                func=m.Attribute(
+                    # e.g. self.assertEqual
+                    value=m.Name("self"),
+                    attr=m.Name(unittest_method),
+                )
+            ),
+            arity=replacement.func.arity,
+            replacement=replacement,
+        )
+        for unittest_method, replacement in _method_map.items()
+    ]
+
+
 class TestTransformer(m.MatcherDecoratableTransformer):
+
+    matchers: List[Rewrite]
+
+    def __init__(self):
+        super(TestTransformer, self).__init__()
+        self.matchers = _build_matchers()
+
     def leave_Call(
         self, original_node: cst.Call, updated_node: cst.BaseExpression
     ) -> cst.BaseExpression:
@@ -56,87 +298,13 @@ class TestTransformer(m.MatcherDecoratableTransformer):
         call = original_node
         args = call.args
 
-        matchers: List[Rewrite] = [
-            # self.assertEqual(a,b) => assert a == b
-            Rewrite(
-                matcher=m.Call(
-                    func=m.Attribute(
-                        value=m.Name("self"), attr=m.Name("assertEqual")
-                    )
-                ),
-                arity=2,
-                replacement=lambda: cst.Assert(
-                    cst.Comparison(
-                        args[0].value,
-                        [cst.ComparisonTarget(cst.Equal(), args[1].value)],
-                    )
-                ),
-            ),
-            # self.assertNotEqual(a,b) => assert a != b
-            Rewrite(
-                m.Call(
-                    func=m.Attribute(
-                        value=m.Name("self"), attr=m.Name("assertNotEqual")
-                    )
-                ),
-                2,
-                lambda: cst.Assert(
-                    cst.Comparison(
-                        args[0].value,
-                        [cst.ComparisonTarget(cst.NotEqual(), args[1].value)],
-                    )
-                ),
-            ),
-            # self.assertTrue(a) => assert a
-            Rewrite(
-                m.Call(
-                    func=m.Attribute(
-                        value=m.Name("self"), attr=m.Name("assertTrue")
-                    )
-                ),
-                1,
-                lambda: cst.Assert(args[0].value),
-            ),
-            # self.assertIsNone(a) => assert a is None
-            Rewrite(
-                m.Call(
-                    func=m.Attribute(
-                        value=m.Name("self"), attr=m.Name("assertIsNone")
-                    )
-                ),
-                1,
-                lambda: cst.Assert(
-                    cst.Comparison(
-                        args[0].value,
-                        [cst.ComparisonTarget(cst.Is(), cst.Name("None"))],
-                    )
-                ),
-            ),
-            # self.assertIsNotNone(a) => assert a is not None
-            Rewrite(
-                m.Call(
-                    func=m.Attribute(
-                        value=m.Name("self"), attr=m.Name("assertIsNotNone")
-                    )
-                ),
-                1,
-                lambda: cst.Assert(
-                    cst.Comparison(
-                        args[0].value,
-                        [
-                            cst.ComparisonTarget(
-                                cst.Is(), cst.parse_expression("not None")
-                            )
-                        ],
-                    )
-                ),
-            ),
-        ]
-
-        for match in matchers:
-            if m.matches(call, match.matcher):
-                if len(args) == match.arity:
-                    return match.replacement()
+        for match in self.matchers:
+            if not m.matches(call, match.matcher):
+                continue
+            if len(args) != match.arity:
+                continue
+            _args = [args[i] for i in range(match.arity)]
+            return match.replacement(*_args)
 
         return updated_node
 
--- tests/data/simple_class.py
+++ tests/data/simple_class.py
@@ -39,9 +39,12 @@ class RewriteMe(TestCase):
 
     def test_success(self):
         self.assertEqual(1, 1)
+        self.assertIsNone(None)
+        self.assertIsNotNone([])
+        self.assertTrue(True)
 
     def test_fail(self):
-        self.assertEqual(1, 2)
+        self.assertNotEqual(1, 2)
 
     def test_doit(self):
         self.assertRegex("herpa", "her.*", "could not find herpa")
@@ -63,3 +66,43 @@ class RewriteMe(TestCase):
         self.failUnless(bool(v2))
         self.failUnless(v3)
         self.failUnless(v4)
+
+    def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+
+    def test_isupper(self):
+        self.assertTrue("FOO".isupper())
+        self.assertFalse("Foo".isupper())
+
+    def test_split(self):
+        s = "hello world"
+        self.assertEqual(s.split(), ["hello", "world"])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+        self.assertRaisesRegex(
+            ValueError, "invalid literal for.*XYZ'$", int, "XYZ"
+        )
+        with self.assertRaisesRegex(ValueError, "literal"):
+            int("XYZ")
+
+    def test_default_widget_size(self):
+        widget = "The widget"
+        self.assertEqual(len(widget), (50, 50))
+
+    def test_even(self):
+        """
+        Test that numbers between 0 and 5 are all even.
+        """
+        for i in range(0, 6):
+            with self.subTest(i=i):
+                self.assertEqual(i % 2, 0)
+
+    def test_warn(self):
+        self.assertWarnsRegex(
+            DeprecationWarning, r"len\(\) is deprecated", len, "XYZ"
+        )
+
+        with self.assertWarnsRegex(RuntimeWarning, "unsafe frobnicating"):
+            len("/etc/passwd")
